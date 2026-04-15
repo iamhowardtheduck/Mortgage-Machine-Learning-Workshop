578ce9dc916fba8d5e933084d485a6a4f282130b
Peter Titov <11168913+iamhowardtheduck@users.noreply.github.com>
2026-04-15 10:59:40 -0400

    Update bootstrap.sh

--- Scripts/bootstrap.sh
+++ Scripts/bootstrap.sh
@@ -1,1 +1,18 @@
-pip install faker --break-system-packages && python /workspace/workshop/Mortgage-Machine-Learning-Workshop/bootstrap-MLv2-WORKSHOP.py --host http://kubernetes-vm:30920 --user sdg --password changeme --kibana-host http://kubernetes-vm:30002 --no-verify-ssl --skip-ml && python /workspace/workshop/Mortgage-Machine-Learning-Workshop/sdg-prime-outlier.py --host http://kubernetes-vm:30920 --user sdg --password changeme --no-verify-ssl --days 30 --traces-per-day 5000 --anomaly-chance 0.05 --backfill
+pip install faker elasticsearch --break-system-packages 
+python /workspace/workshop/Mortgage-Machine-Learning-Workshop/bootstrap-MLv2-WORKSHOP.py \
+  --host http://kubernetes-vm:30920 \
+  --user sdg \
+  --password changeme \
+  --kibana-host http://kubernetes-vm:30002 \
+  --no-verify-ssl \
+  --skip-ml \
+  --dfa-types none
+python /workspace/workshop/Mortgage-Machine-Learning-Workshop/sdg-prime-outlier.py \
+  --host http://kubernetes-vm:30920 \
+  --user sdg \
+  --password changeme \
+  --no-verify-ssl \
+  --days 30 \
+  --traces-per-day 5000 \
+  --anomaly-chance 0.05 \
+  --backfill
